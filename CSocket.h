1a1765f4755e9c22759678e60b1cc6291be70825
Roflane <irasul025@gmail.com>
2025-09-25 01:06:43 +0400

    Push & Update

--- CSocket.h
+++ CSocket.h
@@ -4,15 +4,32 @@
 
 #pragma comment (lib, "Ws2_32.lib")
 
-
 class CSocket {
 private:
     WSADATA _wsaData;
     SOCKET _serverSocket = INVALID_SOCKET;
     addrinfo _hints{};
     addrinfo* _outAddrInfo = nullptr;
+    bool _isServer = false;
 public:
-    explicit CSocket(const char *ip, const char *port, const int sock_type, const int protocol) {
+    explicit CSocket(const char *port, const int sock_type, const int protocol, const bool isServer = true)
+        : _isServer(isServer) {
+        WSAStartup(MAKEWORD(2,2), &_wsaData);
+        ZeroMemory(&_hints, sizeof(_hints));
+
+        _hints.ai_family = AF_INET;
+        _hints.ai_socktype = sock_type;
+        _hints.ai_protocol = protocol;
+
+        if (_isServer) {
+            _hints.ai_flags = AI_PASSIVE;
+            getaddrinfo(nullptr, port, &_hints, &_outAddrInfo);
+        }
+        _serverSocket = CreateSocket();
+    }
+
+    explicit CSocket(const char *ip, const char *port, const int sock_type, const int protocol)
+        : _isServer(false) {
         WSAStartup(MAKEWORD(2,2), &_wsaData);
         ZeroMemory(&_hints, sizeof(_hints));
 
@@ -21,48 +38,75 @@ public:
         _hints.ai_protocol = protocol;
 
         getaddrinfo(ip, port, &_hints, &_outAddrInfo);
-        _serverSocket = socket(_hints.ai_family, _hints.ai_socktype, _hints.ai_protocol);
+        _serverSocket = CreateSocket();
     }
+
     ~CSocket() { Close(); }
 
     // Properties
     SOCKET getSocket() const { return _serverSocket; }
     addrinfo* getAddrInfo() const { return _outAddrInfo; }
     WSADATA getWSAData() const { return _wsaData; }
+    bool isServer() const { return _isServer; }
 
     // Methods
-    [[nodiscard]] int Bind() {
-        _serverSocket = socket(_hints.ai_family, _hints.ai_socktype, _hints.ai_protocol);
-        return bind(_serverSocket, _outAddrInfo->ai_addr, sizeof(addrinfo));
+    [[nodiscard]] SOCKET CreateSocket() {
+        _serverSocket = socket(_outAddrInfo->ai_family, _outAddrInfo->ai_socktype, _outAddrInfo->ai_protocol);
+        return _serverSocket;
     }
 
-     [[nodiscard]] int Listen() const {
-        return listen(_serverSocket, SOMAXCONN);
+    [[nodiscard]] int Bind() const {
+        return bind(_serverSocket, _outAddrInfo->ai_addr, static_cast<int>(_outAddrInfo->ai_addrlen));
+    }
+
+    [[nodiscard]] int Listen(int backlog = SOMAXCONN) const {
+        return listen(_serverSocket, backlog);
     }
 
     [[nodiscard]] int Connect() const {
         return connect(_serverSocket, _outAddrInfo->ai_addr, static_cast<int>(_outAddrInfo->ai_addrlen));
     }
 
-    [[nodiscard]] SOCKET Accept() const {
-        return accept(_serverSocket, _outAddrInfo->ai_addr, reinterpret_cast<int*>(_outAddrInfo->ai_addrlen));
+    [[nodiscard]] SOCKET Accept(sockaddr *addr = nullptr, socklen_t *addrlen = nullptr) const {
+        return accept(_serverSocket, addr, addrlen);
     }
 
-    int Send(const char *buffer, const int length) const {
-        return send(_serverSocket, buffer,  length, NULL);
+    int Send(const char *buffer, const int length, const int flags = 0) const {
+        return send(_serverSocket, buffer, length, flags);
     }
 
-    int Receive(char *buffer, const int length) const {
-        return recv(_serverSocket, buffer, length, NULL);
+    int Receive(char *buffer, const int length, const int flags = 0) const {
+        return recv(_serverSocket, buffer, length, flags);
+    }
+
+    int SendTo(const SOCKET clientSocket, const char *buffer, const int length, const int flags = 0) const {
+        return send(clientSocket, buffer, length, flags);
+    }
+
+    int ReceiveFrom(const SOCKET clientSocket, char *buffer, const int length, const int flags = 0) const {
+        return recv(clientSocket, buffer, length, flags);
     }
 
     [[nodiscard]] int Disconnect() const {
         return shutdown(_serverSocket, SD_BOTH);
     }
 
-    void Close() const {
-        freeaddrinfo(_outAddrInfo);
-        closesocket(_serverSocket);
+    void Close() {
+        if (_serverSocket != INVALID_SOCKET) {
+            closesocket(_serverSocket);
+            _serverSocket = INVALID_SOCKET;
+        }
+        if (_outAddrInfo) {
+            freeaddrinfo(_outAddrInfo);
+            _outAddrInfo = nullptr;
+        }
         WSACleanup();
     }
-};
+
+    static int CloseClient(const SOCKET clientSocket) {
+        if (clientSocket == INVALID_SOCKET) {
+            return SOCKET_ERROR;
+        }
+        return closesocket(clientSocket);
+    }
+};;
---
+++ example_server.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <thread>
+#include <vector>
+#include "CSocket.h"
+
+#pragma comment (lib, "Ws2_32.lib")
+
+#define log_red(fmt, ...) printf("\033[31m" fmt "\033[0m\n", ##__VA_ARGS__)
+#define log_green(fmt, ...) printf("\033[32m" fmt "\033[0m\n", ##__VA_ARGS__)
+#define log_blue(fmt, ...) printf("\033[34m" fmt "\033[0m\n", ##__VA_ARGS__)
+
+#define BUFFER_CAP 1024
+
+int main() {
+    CSocket sock("4773", SOCK_STREAM, IPPROTO_TCP, true);
+
+    SOCKET serverSocket = sock.getSocket();
+
+    if (serverSocket == INVALID_SOCKET) {
+        log_red("Failed to create server socket");
+        return -1;
+    }
+
+    if (const int result = sock.Bind(); result != 0) {
+        log_red("Failed to bind server socket: %d", result);
+        return result;
+    }
+
+    if (const int result = sock.Listen(); result != 0) {
+        log_red("Failed to listen on socket: %d", result);
+        return result;
+    }
+
+    std::vector<SOCKET> clients;
+    std::vector<std::thread> clientThreads;
+
+    std::thread mainThread {[&]() {
+            while (true) {
+                log_blue("Waiting for a client...");
+
+                SOCKET clientSocket = sock.Accept(nullptr, nullptr);
+                if (clientSocket == INVALID_SOCKET) {
+                    log_red("Invalid client socket!");
+                    break;
+                }
+
+                log_green("\nClient accepted!");
+
+                std::thread clientThread ([&, clientSocket](){
+                        try {
+                            std::string buffer = {};
+                            buffer.resize(BUFFER_CAP);
+
+                            const int nicknameData = sock.ReceiveFrom(clientSocket, buffer.data(), BUFFER_CAP);
+                            if (nicknameData > 0) {
+                                const std::string nickname = buffer.substr(0, nicknameData);
+                                log_green("Established connection with %s", nickname.c_str());
+
+                                while (true) {
+                                    const int messageData = sock.ReceiveFrom(clientSocket, buffer.data(), BUFFER_CAP);
+
+                                    if (messageData > 0) {
+                                        const std::string message = buffer.substr(0, messageData);
+                                        log_green("Message from %s: %s", nickname.c_str(), message.c_str());
+                                    }
+                                    else {
+                                        CSocket::CloseClient(clientSocket);
+                                        log_red("Client '%s' has been disconnected!", nickname.c_str());
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    catch (const std::exception &e) {
+                        CSocket::CloseClient(clientSocket);
+                        log_red("Exception: %s", e.what());
+                        log_red("Client disconnected!");
+                    }
+                });
+
+                clients.push_back(clientSocket);
+                clientThreads.push_back(std::move(clientThread));
+            }
+        }
+    };
+
+    mainThread.join();
+
+    for (const auto &client : clients) {
+        CSocket::CloseClient(client);
+    }
+
+    for (auto& thread : clientThreads) {
+        thread.join();
+    }
+}
