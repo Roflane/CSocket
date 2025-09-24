2bc36b6eb6f3207d265cf2d6286d4e10cbf5f71c
Roflane <irasul025@gmail.com>
2025-09-24 19:23:03 +0400

    Push

---
+++ CSocket.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+
+
+class CSocket {
+private:
+    WSADATA _wsaData;
+    SOCKET _serverSocket = INVALID_SOCKET;
+    addrinfo _hints{};
+    addrinfo* _outAddrInfo = nullptr;
+public:
+    explicit CSocket(const char *ip, const char *port, const int sock_type, const int protocol) {
+        WSAStartup(MAKEWORD(2,2), &_wsaData);
+        ZeroMemory(&_hints, sizeof(_hints));
+
+        _hints.ai_family = AF_INET;
+        _hints.ai_socktype = sock_type;
+        _hints.ai_protocol = protocol;
+
+        getaddrinfo(ip, port, &_hints, &_outAddrInfo);
+        _serverSocket = socket(_hints.ai_family, _hints.ai_socktype, _hints.ai_protocol);
+    }
+    ~CSocket() { Close(); }
+
+    // Properties
+    SOCKET getSocket() const { return _serverSocket; }
+    addrinfo* getAddrInfo() const { return _outAddrInfo; }
+    WSADATA getWSAData() const { return _wsaData; }
+
+    // Methods
+    [[nodiscard]] int Bind() {
+        _serverSocket = socket(_hints.ai_family, _hints.ai_socktype, _hints.ai_protocol);
+        return bind(_serverSocket, _outAddrInfo->ai_addr, sizeof(addrinfo));
+    }
+
+     [[nodiscard]] int Listen() const {
+        return listen(_serverSocket, SOMAXCONN);
+    }
+
+    [[nodiscard]] int Connect() const {
+        return connect(_serverSocket, _outAddrInfo->ai_addr, static_cast<int>(_outAddrInfo->ai_addrlen));
+    }
+
+    [[nodiscard]] SOCKET Accept() const {
+        return accept(_serverSocket, _outAddrInfo->ai_addr, reinterpret_cast<int*>(_outAddrInfo->ai_addrlen));
+    }
+
+    int Send(const char *buffer, const int length) const {
+        return send(_serverSocket, buffer,  length, NULL);
+    }
+
+    int Receive(char *buffer, const int length) const {
+        return recv(_serverSocket, buffer, length, NULL);
+    }
+
+    [[nodiscard]] int Disconnect() const {
+        return shutdown(_serverSocket, SD_BOTH);
+    }
+
+    void Close() const {
+        freeaddrinfo(_outAddrInfo);
+        closesocket(_serverSocket);
+        WSACleanup();
+    }
+};
---
+++ example_client.cpp
@@ -0,0 +1,36 @@
+﻿#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include "CSocket.h"
+
+#define LOG(x, ...) printf(x "\n", ##__VA_ARGS__)
+
+int main() {
+    const char *ip = "192.168.31.121";
+    const char *port = "4773";
+    int socket_type = SOCK_STREAM;
+    int protocol = IPPROTO_TCP;
+
+    CSocket sock(ip, port, socket_type, protocol);
+
+    if (sock.getSocket() == INVALID_SOCKET) {
+        printf("Socket creation failed: %d\n", WSAGetLastError());
+    }
+    if (sock.Connect() == SOCKET_ERROR) {
+        printf("Connect failed: %d\n", WSAGetLastError());
+    }
+
+    LOG("Input your nickname: ");
+    std::string nicknameBuffer;
+    std::cin >> nicknameBuffer;
+    const int bytesSent = sock.Send(nicknameBuffer.c_str(), nicknameBuffer.size());
+    LOG("bytes sent: %i\n", bytesSent);
+    LOG("Successfully authorized.");
+
+    while (true) {
+        std::string msgBuffer;
+        std::getline(std::cin, msgBuffer);
+        const int bytesSent2 = sock.Send(msgBuffer.c_str(), msgBuffer.size());
+        LOG("bytes sent: %i\n", bytesSent2);
+    }
+}
